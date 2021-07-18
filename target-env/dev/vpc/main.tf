ae204676e41d5df86122977d0910a9658587b981
Krishna varaprasad <krishna.varaprasad@cradlepoint.com>
2021-07-18 23:41:39 +0530

    adding a code

--- target-env/dev/vpc/main.tf
+++ target-env/dev/vpc/main.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = "0.14.3"
-  backend "s3" {
-    bucket = ""
-    region = "us-east-1"
-  }
+  #backend "s3" {
+  #  bucket = ""
+  #  region = "us-east-1"
+  #}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
--- target-env/dev/vpc/vpc.tf
+++ target-env/dev/vpc/vpc.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 locals {
   name   = "dev"
-  region = ""
+  region = "us-west-2"
   tags = {
     Owner = "krishna.rudraraju"
     Environment = "dev"
@@ -19,15 +19,15 @@ module "dev" {
   enable_dns_support = true
 
   azs = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  public_subnets  = ["10.4.2.0/24", "10.4.3.0/24"]
-  private_subnets = ["10.4.0.0/24", "10.4.1.0/24"]
+  public_subnets  = ["10.2.2.0/24", "10.2.3.0/24"]
+  private_subnets = ["10.2.0.0/24", "10.2.1.0/24"]
   enable_nat_gateway = true 
   single_nat_gateway = true
-  enable_peer_with_vpn = true
+  enable_peer_with_vpn = false
   vpn_peer_id = "vpc-xxxxx"
   vpn_peer_cidr = "10.1.0.0/24"
   vpn_peering_tag = "VPN"
-  enable_peer_with_systems = true
+  enable_peer_with_systems = false
   systems_peer_id = "vpc-xxxx"
   systems_peer_cidr = "10.3.0.0/20"
   systems_peering_tag = "SYSTEMS-Dev"
